5fd3ea8d39b8e0c78ce361d24ef328ad63f4f8e5
Maciej Swiderski <maciejswiderski@wp.pl>
2018-02-11 12:04:02 +0100

    next

--- src/com/sda/javaFX/Main.java
+++ src/com/sda/javaFX/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 250, 375));
         primaryStage.show();
-        primaryStage.setFullScreen(true);
+        //primaryStage.setFullScreen(true);
     }
 
 
